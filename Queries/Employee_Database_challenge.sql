a9218fd04fff916cc8547b9162584235213cf648
yycheung <yycheung@Galaxy.local>
2022-03-15 23:24:20 -0700

    The Employees Eligible for the Mentorship Program

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -95,4 +95,30 @@ FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 
+-- create a Mentorship Eligibility table that holds the employees 
+-- who are eligible to participate in a mentorship program.
+
+SELECT DISTINCT ON(e.emp_no)
+e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+
+
+
+
+
 
